c6a42c3d9686d6a2a023237b711ab3722b7da90a
Maryen <voujum@gmail.com>
2023-02-23 12:31:13 +0100

    Challenge for 23.02.23 v2 refactoring

--- 6th kyu/are-they-the-same.js
+++ 6th kyu/are-they-the-same.js
@@ -11,14 +11,8 @@ let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
 
 function comp(array1, array2){
   if (array1 == null || array2 == null ) return false;
-  
-  array1.sort((a, b) => a - b);
-  array2.sort((a, b) => a - b);
-  
-  return array2.every((item, i) => {
-    if (array1[i] == 0 && item == 0) return true;
-    return item / array1[i] == array1[i];
-  });
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array2.every((item, i) => array1[i] * array1[i] == item);
 }
 
 comp(a, b); // true
