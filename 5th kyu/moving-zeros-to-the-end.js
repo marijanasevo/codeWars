4ae4b013d3ddd257df7286f82e46e6affa4e29db
Maryen <voujum@gmail.com>
2023-02-24 23:28:29 +0100

    Challenge for 24.02.23

---
+++ 5th kyu/moving-zeros-to-the-end.js
@@ -0,0 +1,14 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+// Doing this one sleepy
+
+function moveZeros(arr) {
+  return arr.sort((a, b) => {
+    (a === 0) ? a = 1 : a = 0;
+    (b === 0) ? b = 1 : b = 0;
+
+    return a - b;
+  });
+}
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]); // returns[false,1,1,2,1,3,"a",0,0]
+
