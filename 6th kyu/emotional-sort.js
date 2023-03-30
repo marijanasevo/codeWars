767c49c5b97b25331a8672b0d5647165266e3a83
Maryen <voujum@gmail.com>
2023-03-30 12:18:47 +0200

    Challenge 30.03.23

---
+++ 6th kyu/emotional-sort.js
@@ -0,0 +1,43 @@
+// You'll have a function called "sortEmotions" that will return an array of emotions sorted. It has two parameters, the first parameter called "arr" will expect an array of emotions where an emotion will be one of the following:
+
+// :D -> Super Happy
+// :) -> Happy
+// :| -> Normal
+// :( -> Sad
+// T_T -> Super Sad
+// Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
+
+// And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
+
+// Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
+// Super Happy -> Happy -> Normal -> Sad -> Super Sad
+
+// If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
+// Super Sad -> Sad -> Normal -> Happy -> Super Happy
+
+// Example:
+
+// arr = [':D', ':|', ':)', ':(', ':D']
+// sortEmotions(arr, true) // [ ':D', ':D', ':)', ':|', ':(' ]
+// sortEmotions(arr, false) // [ ':(', ':|', ':)', ':D', ':D' ]
+// More in test cases!
+
+// Notes:
+
+// The array could be empty, in that case return the same empty array ¯\_( ツ )_/¯
+
+
+
+function sortEmotions(arr, order){
+  let emojis = { 'T_T': 5, ':(': 4, ':|': 3, ':)': 2, ':D': 1 }
+
+  return arr.sort((emojiA, emojiB) => {
+    let A = emojis[emojiA];
+    let B = emojis[emojiB];
+    return (order) ? A - B : B - A;
+  });
+}
+
+sortEmotions([ ':D', 'T_T', ':D', ':(' ], true); // [ ':D', ':D', ':(', 'T_T' ])
+sortEmotions([ 'T_T', ':D', ':(', ':(' ], true); // [ ':D', ':(', ':(', 'T_T' ])
+sortEmotions([ ':)', 'T_T', ':)', ':D', ':D' ], true); // [ ':D', ':D', ':)', ':)', 'T_T' ])
